452c1afae16778e66bd1d22824328cffe7004756
swandevson <shai4956@gmail.com>
2024-04-11 00:36:13 +0900

    feat: jwt 발급 및 인증 구현

---
+++ src/main/java/com/zigtong/clientserver/security/constant/HeaderConstant.java
@@ -0,0 +1,10 @@
+package com.zigtong.clientserver.security.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class HeaderConstant {
+	public static final String BEARER_PREFIX = "Bearer";
+
+}
