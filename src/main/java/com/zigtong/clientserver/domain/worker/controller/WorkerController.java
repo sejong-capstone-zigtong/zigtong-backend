c4707a6631c6c8e621288ae50797ad0d5fff3867
swandevson <shai4956@gmail.com>
2024-05-01 13:12:34 +0900

    chore: swagger controller 스펙 설명 작성

--- src/main/java/com/zigtong/clientserver/domain/worker/controller/WorkerController.java
+++ src/main/java/com/zigtong/clientserver/domain/worker/controller/WorkerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zigtong.clientserver.domain.worker.dto.request.WorkerSignUpRequest;
 import com.zigtong.clientserver.domain.worker.service.WorkerService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class WorkerController {
 	private final WorkerService workerService;
 
+	@Operation(summary = "회원가입", description = "일반 사용자 회원가입을 진행합니다.")
 	@PostMapping("/sign-up")
 	public ResponseEntity<Void> signUp(@RequestBody WorkerSignUpRequest request, HttpServletRequest servletRequest) {
 		HttpSession session = servletRequest.getSession();
